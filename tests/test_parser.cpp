a4a963eb5a38e393691e4bb9939727dc4ed26ac9
Luke Fasanello <lbf5144@psu.edu>
2025-06-19 20:46:49 -0400

    Fixing test case as well

--- tests/test_parser.cpp
+++ tests/test_parser.cpp
@@ -1,14 +1,16 @@
 #include <gtest/gtest.h>
 #include <fstream>
+#include <filesystem> 
 #include "../Classes/ItchParser.hpp"
+
+namespace fs = std::filesystem;
 
 TEST(ParserSmoke, LogFileIsNotEmpty)
 {
     const std::string logPath = "../logging.txt";
     ItchParser parser;
     parser.parseFile("tests/sample.itch");   // whatever your API is
-
-    namespace fs = std::filesystem;
+    
     ASSERT_TRUE(fs::exists(logPath));
     ASSERT_GT(fs::file_size(logPath), 0);
 }
